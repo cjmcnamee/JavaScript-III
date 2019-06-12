4fadef676403fc9c1a68abc5fb6f67b91f0d4305
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-06-12 17:39:22 -0400

    prototypes added

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -13,19 +13,19 @@
     this.createdAt = Date();
     this.name = att.name;
     this.dimensions = att.dimensions // (These represent the character's size in the video game)
-    this.destroy = function () {
+    }
+  Object.prototype.destroy = function () {
     return `${this.name} was removed from game.`; // prototype method that returns: `${this.name} was removed from the game.`
-    };
-  }
+  };
 
   // === CharacterStats ===
   function CharacterStats(att) {
     GameObject.call(this, att);
-    this.healthPoints = att.healthPoints,
-    this.takeDamage = function () {
-      return `${this.name} took damage.`;  // prototype method -> returns the string '<object name> took damage.'
-    };
-  }
+    this.healthPoints = att.healthPoints
+    }
+  Object.prototype.takeDamage = function () {
+    return `${this.name} took damage.`;  // prototype method -> returns the string '<object name> took damage.'
+  };
     // * should inherit destroy() from GameObject's prototype
 
   // === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -34,10 +34,10 @@
     this.team = att.team
     this.weapons = att.weapons
     this.language = att.language
-    this.greet = function () {
-      return `${this.name} offers a greeting in ${this.language}.`; // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-    };
-  }
+    }
+  Object.prototype.greet = function () {
+    return `${this.name} offers a greeting in ${this.language}.`; // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+  };
     // * should inherit destroy() from GameObject through CharacterStats
     // * should inherit takeDamage() from CharacterStats
 
