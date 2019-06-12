7a32f50f802807a3f63dded6cc7ef9c7cc3d2605
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-06-12 17:20:22 -0400

    finished prototypes.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -1,38 +1,47 @@
 /*
   Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
 
-  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.  
+  In this file you will be creating three constructor functions: GameObject, CharacterStats, Humanoid.
 
   At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
+
   Each constructor function has unique properties and methods that are defined in their block comments below:
 */
-  
-/*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-*/
 
-/*
-  === CharacterStats ===
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
-  * should inherit destroy() from GameObject's prototype
-*/
+  // === GameObject ===
+  function GameObject(att) {
+    this.createdAt = Date();
+    this.name = att.name;
+    this.dimensions = att.dimensions // (These represent the character's size in the video game)
+    this.destroy = function () {
+    return `${this.name} was removed from game.`; // prototype method that returns: `${this.name} was removed from the game.`
+    };
+  }
 
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject through CharacterStats
-  * should inherit takeDamage() from CharacterStats
-*/
- 
+  // === CharacterStats ===
+  function CharacterStats(att) {
+    GameObject.call(this, att);
+    this.healthPoints = att.healthPoints,
+    this.takeDamage = function () {
+      return `${this.name} took damage.`;  // prototype method -> returns the string '<object name> took damage.'
+    };
+  }
+    // * should inherit destroy() from GameObject's prototype
+
+  // === Humanoid (Having an appearance or character resembling that of a human.) ===
+  function Humanoid(att) {
+    CharacterStats.call(this, att);
+    this.team = att.team
+    this.weapons = att.weapons
+    this.language = att.language
+    this.greet = function () {
+      return `${this.name} offers a greeting in ${this.language}.`; // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+    };
+  }
+    // * should inherit destroy() from GameObject through CharacterStats
+    // * should inherit takeDamage() from CharacterStats
+
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -41,7 +50,7 @@
 
 // Test you work by un-commenting these 3 objects and the list of console logs below:
 
-/*
+
   const mage = new Humanoid({
     createdAt: new Date(),
     dimensions: {
@@ -102,9 +111,9 @@
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
   console.log(mage.takeDamage()); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
 
-  // Stretch task: 
-  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+
+  // Stretch task:
+  // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
